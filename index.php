d1dd39a43a4f51852fd50f9d3ead48836040d6b7
Mikhail <36668926+Cheglok@users.noreply.github.com>
2019-08-11 22:57:51 +0500

    Task 5 done

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+function renderTemplate($page, $content="")
+{
+    ob_start();
+    include $page . ".php";
+    return ob_get_clean();
+}
+
+echo renderTemplate(layout, renderTemplate(header) . renderTemplate(menu)
+    . renderTemplate(hello). renderTemplate(footer));
---
+++ layout.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport">
+    <title>Template</title>
+</head>
+<body>
+<?=$content?>
+</body>
+</html>
