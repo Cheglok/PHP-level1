84e0eeedea839ac5485bdde35fefc3a77f959181
Mikhail <36668926+Cheglok@users.noreply.github.com>
2019-10-05 10:45:01 +0500

    Добавлены файлы для задания 5, решено с вынесением меню в отдельный шаблон

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo renderTemplate('layout', renderTemplate('menu'), renderTemplate("index"));
+
+
+function renderTemplate($page, $menu = '', $content = '')
+{
+    ob_start();
+    include "./templates/{$page}.php";
+    return ob_get_clean();
+}
