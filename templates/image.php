1f383970be922fad650840c8647139c14f0c6744
Mikhail <36668926+Cheglok@users.noreply.github.com>
2019-10-21 21:54:11 +0500

    clean area

--- templates/image.php
+++
@@ -1,9 +0,0 @@
-<h2>Изображение</h2>
-<div>
-    <?=$params['a']?>
-    <p>
-        Просмотров <?=mysqli_fetch_assoc(@mysqli_query($db,
-            "SELECT `likes` FROM `gallerylist` WHERE `fileName`= '$image'"))['likes']?>
-    </p>
-    <img src="images/gallery_img/big/<?=$params['image']?>"/>
-</div>
--- templates/index.php
+++
@@ -1,2 +0,0 @@
-Добро пожаловать <?=$login?> в магазин <?=$name?> <br>
-<img src="images/gallery_img/big/01.jpg" width="300">
