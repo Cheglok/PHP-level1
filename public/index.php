dbf080479854c3695a110d1b630bd5b7817bb92a
Mikhail <36668926+Cheglok@users.noreply.github.com>
2019-11-02 21:38:41 +0500

    Новый репозиторий

--- public/index.php
+++ public/index.php
@@ -1,38 +1,37 @@
 <?php
-echo "hello";
-//use app\models\{Product, Users, Basket, Orders, Feedback};
-//use app\engine\{Db, Autoload};
-//
-//include realpath("../config/config.php");
-//include realpath("../engine/Autoload.php");
-//
-//spl_autoload_register([new Autoload(), 'loadClass']);
-//
-//$db = new Db();
-//
-//$product = new Product($db);
-//$users = new Users($db);
-//$basket = new Basket($db);
-//$orders = new Orders($db);
-//$feedback = new Feedback($db);
-//
-//function foo($model) {
-//    return $model->getTableName();
-//}
-//
-//echo "<br>" . foo($product);
-//echo $product->getOne(1) . "<br>";
-//
-//echo foo($users);
-//echo $users->getOne(1) . "<br>";
-//
-//echo foo($basket);
-//echo $basket->getOne(1) . "<br>";
-//
-//echo foo($orders);
-//echo $orders->getAll(1) . "<br>";
-//
-//echo foo($feedback);
-//echo $feedback->getOne(1) . "<br>";
+use app\models\{Product, Users, Basket, Orders, Feedback};
+use app\engine\{Db, Autoload};
+
+include realpath("../config/config.php");
+include realpath("../engine/Autoload.php");
+
+spl_autoload_register([new Autoload(), 'loadClass']);
+
+$db = new Db();
+
+$product = new Product($db);
+$users = new Users($db);
+$basket = new Basket($db);
+$orders = new Orders($db);
+$feedback = new Feedback($db);
+
+function foo($model) {
+    return $model->getTableName();
+}
+
+echo "<br>" . foo($product);
+echo $product->getOne(1) . "<br>";
+
+echo foo($users);
+echo $users->getOne(1) . "<br>";
+
+echo foo($basket);
+echo $basket->getOne(1) . "<br>";
+
+echo foo($orders);
+echo $orders->getAll(1) . "<br>";
+
+echo foo($feedback);
+echo $feedback->getOne(1) . "<br>";
 
 
